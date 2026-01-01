8122fcaff3ed87b20d912b783381866eade72cf8
Takahiro Ogawa <boroney1@gmail.com>
2026-01-01 13:23:10 +0900

    refactor prisma

--- lib/actions/product.actions.ts
+++ lib/actions/product.actions.ts
@@ -1,16 +1,7 @@
 "use server";
 
-import { PrismaClient } from "@/lib/generated/prisma/client";
-import { PrismaPg } from "@prisma/adapter-pg";
+import { prisma } from "@/db/prisma";
 import { convertToPlainObject } from "@/lib/utils";
-
-const adapter = new PrismaPg({
-  connectionString: process.env.DATABASE_URL,
-});
-
-const prisma = new PrismaClient({
-  adapter,
-});
 
 // Get latest products
 export async function getLatestProducts() {
